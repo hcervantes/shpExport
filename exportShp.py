04f024203cd3a358f0be6c395762a64a4e0a1470
Hector Cervantes <hector_cervantes@msn.com>
2013-06-12 10:50:26 -0400

    Added projection files 102100.prj and 4269.prj.  These are the most common projections used by web map services. After shapefile is saved, the projection file is copied to shapefile. Added sample line, point, and polygon ArcGIS service query response.  This is the response to expect from a map feature service call, and is what is exported to a shapefile

--- exportShp.py
+++ exportShp.py
@@ -8,8 +8,37 @@ Compatible with Python versions 2.4-3.x
 """
 from datetime import datetime
 import sys, os, json
-import shapefile
+import shapefile, shutil
 
+
+def LoadDataFile(jsonFile):
+
+    with open(jsonFile) as f:
+        jsonStr = ''
+        for line in f:
+            jsonStr = jsonStr + line
+        global jsonObject
+        jsonObject = json.loads(jsonStr)
+        print jsonStr
+        
+def SaveShape(w):
+    # save the shapefile
+    # Create unique name
+    dt = datetime.now()
+    fileSafeStr = dt.strftime("%Y%m%d_%H%M%S")
+    w.save('shapefiles/' + fileSafeStr)
+    # copy WebMercator projection
+    shutil.copy('4269.prj', 'shapefiles/' + fileSafeStr + '.prj')
+    print 'Created shapefile ' + fileSafeStr + '.shp'
+    
+def WriteAttributes(features, w):
+        # Itererate over the features to create attributes
+    for feature in features:
+        # Add the records
+        print 'creating attributes'
+        rec = feature["attributes"].values()
+        w.record(*rec)
+        
 def WriteFields(fields, w):
     # Write the fields
    
@@ -39,188 +68,57 @@ def WriteFields(fields, w):
             w.field(str(field["name"]), str(fieldType))
         print 'created field: ' + field['name'] + ' - fieldType: ' + fieldType
 def WritePoints(features, fields):
-    w = shapefile.Writer(shapefile.POINT)
-    
+    w = shapefile.Writer(shapefile.POINT)    
     # Itererate over the features to create geometries
     for feature in features:        
         # Write the geometries
-        print "creating geometry "
         x = feature["geometry"]["x"]
         y = feature["geometry"]["y"]
-        print str(x) + ',' + str(y)
         w.point(x, y)
         #w.point(37.7793, -122.4192)
     # Create the fields
     WriteFields(fields, w)
-    #w.field('FIRST_FLD')
-    #w.field('SECOND_FLD', 'C', '40')
-    # Itererate over the features to create attributes
-    for feature in features:
-        # Add the records
-        print 'creating attributes'
-        rec = feature["attributes"].values()
-        print rec
-        w.record(*rec)
-        #w.record('crap', 'crap2')
-    # save the shapefile
-    # Create unique name
-    dt = datetime.now()
-    fileSafeStr = dt.strftime("%Y%m%d_%H%M%S")
-    w.save('shapefiles/test/poly' + fileSafeStr)
-    print 'Created shapefile ' + fileSafeStr + '.shp'
+    # Populate Attributes
+    WriteAttributes(features, w)
+    # Save the shapefile
+    SaveShape(w)
         
 def WritePolygons(features, fields):
     w = shapefile.Writer(shapefile.POLYGON)
-    # Create the fields
-    WriteFields(fields, w)
+    
     # Itererate over the features to create geometries
     for feature in features:        
         # Write the geometries
-        print "creating geometry "
-        print feature["geometry"]["rings"]
         geom = feature["geometry"]["rings"]
         w.poly(parts=geom)
-    # Itererate over the features to create attributes
-        # Add the records
-        print 'creating attributes'
-        rec = feature["attributes"].values()
-        print rec
-        w.record(*rec)
-    # save the shapefile
-    # Create unique name
-    dt = datetime.now()
-    fileSafeStr = dt.strftime("%Y%m%d_%H%M%S")
-    w.save('shapefiles/test/poly' + fileSafeStr)
-        
-def WritePolylines(features, fields):
-    w = shapefile.Writer(shapefile.POLYGON)
     # Create the fields
     WriteFields(fields, w)
+    # Populate Attributes
+    WriteAttributes(features, w)
+    # Save the shapefile
+    SaveShape(w)
+
+        
+def WritePolylines(features, fields):
+    w = shapefile.Writer(shapefile.POLYLINE)
     # Itererate over the features to create geometries and attributes
     for feature in features:        
         # Write the geometries
-        geom = feature["geometry"]
+        geom = feature["geometry"]["paths"]
         w.poly(parts=geom)
                 
-        # Add the records
-        rec = feature["attributes"]
-        w.record(rec)
-        
-        # save the shapefile
-        w.save('shapefiles/test/poly')
+    # Create the fields
+    WriteFields(fields, w)
+    # Populate Attributes
+    WriteAttributes(features, w)
+    # Save the shapefile
+    SaveShape(w)
 
 # Main Entry        
-if (len(sys.argv) == 0):
-    print 'Expected Featureset as argument'
-    exit
-jsonInput = sys.argv[0]
-
-jsonInput = u'''{
- "displayFieldName": "STATE_FIPS",
- "fieldAliases": {
-  "STATE_FIPS": "STATE_FIPS"
- },
- "geometryType": "esriGeometryPoint",
- "spatialReference": {
-  "wkid": 4269,
-  "latestWkid": 4269
- },
- "fields": [
-  {
-   "name": "STATE_FIPS",
-   "type": "esriFieldTypeString",
-   "alias": "STATE_FIPS",
-   "length": 2
-  }
- ],
- "features": [
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.95355599980951,
-    "y": 32.196335999706662
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.929658000316408,
-    "y": 32.198095999930501
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.922701999697495,
-    "y": 32.209736909031164
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.942107999784298,
-    "y": 32.218989091073752
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.916962000296053,
-    "y": 32.194589090913894
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.979262909155921,
-    "y": 32.174601090967599
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -87.983266000225683,
-    "y": 32.174265999975944
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -88.008253999622355,
-    "y": 32.19314709096551
-   }
-  },
-  {
-   "attributes": {
-    "STATE_FIPS": "01"
-   },
-   "geometry": {
-    "x": -88.010408000029145,
-    "y": 32.227608000209102
-   }
-  }
- ]
-}'''
-
-jsonObject = json.loads(jsonInput)
+LoadDataFile('lineFeatures.json')
 
 # Determine geometry type
+print jsonObject["geometryType"]
 geomType = jsonObject["geometryType"]
 geomType = geomType.upper()
 
